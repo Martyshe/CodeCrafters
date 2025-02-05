8fec2c3433acaa9df363e88d9d77676fbcbf9590
Martyshe <40313551+Martyshe@users.noreply.github.com>
2025-02-05 10:34:08 +0100

    transit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Categories from './components/main/categories/Categories';
 
 
 
 function App() {
-  
-  useEffect(() => {
-    fetch('http://localhost:3333/categories/all') 
-      .then(response => response.json()) 
-      .then(data => console.log(data))
-      .catch(error => console.error('Ошибка:', error));
-  }, []);
-  return (
-    <Categories />
+  return(
+     <Categories /> 
     // <Router>
     //   <Routes>
     //     {/* <Route path="/" element={<Home />} /> */}
     //   </Routes>
     // </Router>
+  
+    
   );
 }
 

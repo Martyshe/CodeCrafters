3d5da0ae4de2b7ccd948db5a0a7814cce6aa6cee
Martyshe <40313551+Martyshe@users.noreply.github.com>
2025-02-10 19:04:42 +0100

    transit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import NavMenu from './components/header/navMenu/Navmenu';
+import BannerSection from './components/header/bannerSection/BannerSection';
 import Categories from './components/main/categories/Categories';
+import { DiscountForm } from './components/main/discountForm/DiscountForm';
+import SaleSection from './components/main/saleSection/SaleSection';
+import ContactSection from './components/footer/contactSection/ContactSection';
+
 
 
 
 function App() {
-  return(
-     <Categories /> 
-    // <Router>
-    //   <Routes>
-    //     {/* <Route path="/" element={<Home />} /> */}
-    //   </Routes>
-    // </Router>
-  
-    
+  return (
+    <>
+    <NavMenu />
+    <BannerSection />
+    <Categories />
+    <DiscountForm/>
+    <SaleSection />
+    <ContactSection/>
+    </>
+   
   );
 }
-
 export default App;

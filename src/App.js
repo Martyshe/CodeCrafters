e9c30ca268cc7ea40010d90704c68fc7844c6c2a
Karyna Keduk <karynakeduk@MacBook-Air-Karyna.local>
2025-02-10 19:32:44 +0100

    transit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import NavMenu from './components/header/navMenu/Navmenu';
+import BannerSection from './components/header/bannerSection/BannerSection';
+import Categories from './components/main/categories/Categories';
 import { DiscountForm } from './components/main/discountForm/DiscountForm';
-import './App.css'
+import SaleSection from './components/main/saleSection/SaleSection';
+import ContactSection from './components/footer/contactSection/ContactSection';
+
+
 
 
 function App() {
   return (
+    <>
+    <NavMenu />
+    <BannerSection />
+    <Categories />
     <DiscountForm/>
-    // <Router>
-    //   <Routes>
-    //     {/* <Route path="/" element={<Home />} /> */}
-    //   </Routes>
-    // </Router>
+    <SaleSection />
+    <ContactSection/>
+    </>
+   
   );
 }
-
-export default App;
+export default App

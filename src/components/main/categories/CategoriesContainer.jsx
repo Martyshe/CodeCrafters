59113c081205a9e8a5bb2102dec7848b83007e8a
Martyshe <40313551+Martyshe@users.noreply.github.com>
2025-02-22 16:16:59 +0100

    changed styles in cart

--- src/components/main/categories/CategoriesContainer.jsx
+++ src/components/main/categories/CategoriesContainer.jsx
@@ -1,58 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import s from "./CategoriesContainer.module.css";
-// import CategoryCard from "../../categoryCard/CategoryCard";
-// import { back } from "../../../constants";
-// import { Link } from "react-router-dom";
-
-// export default function CategoriesContainer({ amount }) {
-//   const [category, setCategory] = useState([]);
-//   useEffect(() => {
-//     fetch(`${back}/categories/all`)
-//       .then((response) => response.json())
-//       .then((data) => {
-//         // Оставляем первые "amount" категории и запихиваем в State
-//         const slicedCategories = data.slice(0, amount);
-
-//         setCategory(slicedCategories);
-//       })
-//       .catch(() => alert("Перезагрузите страницу"));
-//   }, []);
-
-// //   return (
-// //     <div className={s.mainContainer}>
-// //       <div className={s.categoriesContainer}>
-// //         {category.map((el) => (
-// //           <Link key={el.id} to={`/productsByCategory/${el.id}?title=${el.title}`}>
-// //           <CategoryCard {...el} />
-// //         </Link>
-// //         ))}
-// //       </div>
-// //     </div>
-// //   );
-// // }
-
-// return (
-//   <div className={s.mainContainer}>
-//     <div className={s.categoriesContainer}>
-//       {category.map((el) => {
-//         // Проверяем, находится ли пользователь в "/Categories"
-//         const isCategoriesPage = location.pathname.includes("/Categories");
-//         const linkTo = isCategoriesPage
-//           ? `/productsByCategory/${el.id}?title=${el.title}`
-//           : `/Categories/${el.title}?id=${el.id}`;
-
-//         return (
-//           <Link key={el.id} to={linkTo}>
-//             <CategoryCard {...el} />
-//           </Link>
-//         );
-//       })}
-//     </div>
-//   </div>
-// );
-// };
-
-
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom"; // Добавили useLocation
 import s from "./CategoriesContainer.module.css";
@@ -83,7 +28,7 @@ export default function CategoriesContainer({ amount }) {
             : `/Categories/${el.title}?id=${el.id}`;
 
           return (
-            <Link key={el.id} to={linkTo}>
+            <Link key={el.id} to={`/categories/${el.id}?title=${el.title}`}>
               <CategoryCard {...el} />
             </Link>
           );
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,28 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AllProductsPage from '../pages/AllProductsPage'; // Убедитесь, что импорт правильный
+import AllProductsPage from '../pages/AllProductsPage';
 import HomePage from '../pages/HomePage';
 import AllSalesPage from '../pages/AllSalesPage';
 import CategoriesPage from '../pages/categoriesPage/CategoriesPage';
-// import ProductsByCategory from '../pages/ProductsByCategory';
 import Breadcrumbs from '../components/breadcrumbs/Breadcrumbs';
 import Cart from '../components/cart/Cart';
 import ProductDetails from '../components/productDetails/ProductDetails';
-
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { setProducts } from '../redux/productsSlice';
-import ProductList from '../components/productList/ProductList';
 import Categories from '../pages/Categories';
 
-// Уберите повторное объявление компонента AllProductsPage
-// Этот компонент должен быть только один раз (в файле productsSlice, например)
 const AppRouter = () => (
     <Router>
         <Breadcrumbs />
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/categories" element={<CategoriesPage />} />
-            <Route path="/all-products" element={<AllProductsPage />} /> {/* Используем уже импортированный компонент */}
+            <Route path="/all-products" element={<AllProductsPage />} />
             <Route path="/all-sales" element={<AllSalesPage />} />
             <Route path="/categories/:categoryId" element={<Categories />} />
             <Route path="/cart" element={<Cart />} />

2d16eb103fa823ba6d39f53c73d4250d098d5d66
Karyna Keduk <karynakeduk@MacBook-Air-Karyna.local>
2025-02-26 20:11:05 +0100

    Skeleton

--- src/pages/AllSalesPage.jsx
+++ src/pages/AllSalesPage.jsx
@@ -1,28 +1,39 @@
 
-
 import React, { useEffect, useState } from "react";
 import SaleSection from "../components/main/saleSection/SaleSection";
 import { useProductFilter } from "../components/productsFilter/useProductFilter";
 import ProductsFilter from "../components/productsFilter/ProductsFilter";
-import { back } from "../constants"; // Убедитесь, что путь к константам правильный
+import { back } from "../constants";
+import Skeleton from "../components/skeleton/Skeleton";
+
 
 export default function AllSalesPage() {
   const [allProducts, setAllProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [showSkeleton, setShowSkeleton] = useState(true); // Состояние для управления скелетоном
 
   // Загрузка всех продуктов с сервера
   useEffect(() => {
-    fetch(`${back}/products/all`)
-      .then((response) => response.json())
-      .then((data) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${back}/products/all`);
+        const data = await response.json();
         setAllProducts(data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
+
+        // Устанавливаем задержку в 2 секунды перед скрытием скелетона
+        setTimeout(() => {
+          setIsLoading(false);
+          setShowSkeleton(false);
+        }, 2000);
+      } catch (error) {
         setError(error);
         setIsLoading(false);
-      });
+        setShowSkeleton(false);
+      }
+    };
+
+    fetchData();
   }, []);
 
   // Инициализация начальных фильтров
@@ -40,18 +51,13 @@ export default function AllSalesPage() {
   const discountedProducts = filteredProducts.filter(
     (product) => product.discont_price !== null
   );
-  
 
   const style = {
     maxWidth: "100%",
     padding: "2rem",
-    paddingLeft:'2.25rem',
+    paddingLeft: "2.25rem",
     color: "#424436",
   };
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
   if (error) {
     return <div>Error: {error.message}</div>;
@@ -59,14 +65,24 @@ export default function AllSalesPage() {
 
   return (
     <div>
-      <h2 style={style}>Discounted items</h2>
-      {/* Передаем handleFilterChange в компонент ProductsFilter и скрываем кнопку "Discounted items" */}
-      <ProductsFilter onFilterChange={handleFilterChange} hideDiscountedCheckbox    filterStyle={{
-    paddingLeft: "2.25rem",
-    borderRadius: "8px",
-  }}/>
+    <h2 style={style}>Discounted items</h2>
+    {/* Передаем handleFilterChange в компонент ProductsFilter и скрываем кнопку "Discounted items" */}
+    <ProductsFilter
+      onFilterChange={handleFilterChange}
+      hideDiscountedCheckbox
+      filterStyle={{
+        paddingLeft: "2.25rem",
+        borderRadius: "8px",
+      }}
+    />
+    {/* Отображаем скелетон, если showSkeleton равно true */}
+    {showSkeleton ? (
+      <div style={{ paddingLeft: "2rem" , paddingRight: "2rem"}}> {/* Добавляем padding только для скелетона */}
+        <Skeleton />
+      </div>
+    ) : (
       <SaleSection products={discountedProducts} amount={discountedProducts.length} />
-    </div>
+    )}
+  </div>
   );
 }
-
